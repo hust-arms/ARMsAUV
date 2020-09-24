c2a1e50973a73888fe2a44555cd665418763f919
wangzhao9562 <wangzhao9562@gmail.com>
2020-09-24 13:32:46 +0800

    Add positional PID control

--- armsauv_control/src/clf_los_controller.cpp
+++ armsauv_control/src/clf_los_controller.cpp
@@ -38,13 +38,12 @@
 
         std::cout << "[pid_controller]: " << "k: " << line_.k_ << " b: " << line_.b_ << " is_rev: " << line_.is_reverse_ << std::endl;
         // std::cout << "dy err: " << los_ctrl_param_.dy_err_ << std::endl;
-
          
         double target_y = line_.k_ * x + line_.b_;
         double line_dir = std::atan2(line_.end_y_ - line_.start_y_, line_.end_x_ - line_.start_x_);
  
         double ye = (y - target_y) * std::cos(line_dir);
-        double ref_phi =  -std::atan(ye / factor_);
+        double ref_phi =  -std::atan2(ye, factor_);
 
         if(line_.is_reverse_)
         {
@@ -57,6 +56,8 @@
         // det_phi_ = yaw - (line_dir - ref_phi);
 
         det_phi_ = ref_phi - (yaw - line_dir);
+	
+	// det_phi_ = line_dir - yaw;
 
         if(det_phi_ > PI){
             det_phi_ = det_phi_ - 2 * PI;
@@ -67,12 +68,16 @@
         }
         
         std::cout << "ye: " << ye << " line_dir: " << line_dir << " ref_phi: " << ref_phi << " yaw: " << yaw << " det_phi: " << det_phi_ << std::endl;
-        
-        double r = this->los_ctrl_param_.kp_ * (det_phi_ - det_last_) + this->los_ctrl_param_.ki_ * det_phi_ +
-            this->los_ctrl_param_.kd_ * (det_phi_ - 2 * det_last_ + det_pre_);
-        if(fabs(r - output_last_) > 1.0){
+        	
+        double r = this->los_ctrl_param_.kp_ * (det_phi_ - det_last_) + this->los_ctrl_param_.ki_ * det_phi_ + this->los_ctrl_param_.kd_ * (det_phi_ - 2 * det_last_ + det_pre_);
+	this->det_sum_ += det_phi_;
+	// double r = this->los_ctrl_param_.kp_ * det_phi_ + this->los_ctrl_param_.ki_ * this->det_sum_ + this->los_ctrl_param_.kd_ * (this->det_phi_ - this->det_last_);
+
+        /*
+	if(fabs(r - output_last_) > 1.0){
 	    r = output_last_;
 	};
+	*/
 
 	det_pre_ = det_last_;
 	det_last_ = det_phi_;

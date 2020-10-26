959187df6627832de04c88f60f78b110ae1ef645
wangzhao9562 <wangzhao9562@gmail.com>
2020-10-25 23:57:56 -0700

    Add disturbance generator

---
+++ docking_disturbance_analysis/disturb_gen_comp.m
@@ -0,0 +1,81 @@
+function [a_xdamp, a_ydamp, a_zdamp, is_qt_disturb] = disturb_gen(x_qt, y_qt, z_qt, x_auv, y_auv, z_auv, t)
+%% 尾流轴向扰动最大强度
+a_m = 0.005;
+%% 海流轴向扰动最大强度
+a_m_o_fixed = 0.0005;
+a_m_o_sin = 0.0002;
+a_m_o_rand = 0.001;
+%% 扰动最大作用距离
+x0 = 15;
+y0 = 4;
+z0 = 4;
+dtheshold = sqrt(x0 * x0 + y0 * y0 + z0 * z0);
+%% 产生尾流x，y，z轴向扰动(-a_m,a_m)
+%a_x = a_m * (rand(1)*(-2)+1);
+%a_y = a_m * (rand(1)*(-2)+1);
+%a_z = a_m * (rand(1)*(-2)+1);
+a_x = a_m;
+a_y = a_m;
+a_z = a_m;
+is_qt_disturb = 1;
+%% 根据潜艇尾部到AUV某处距离计算扰动强度
+dx_mah = abs(x_qt - x_auv);
+dy_mah = abs(y_qt - y_auv);
+dz_mah = abs(z_qt - z_auv);
+dx_euc = sqrt(dx_mah * dx_mah);
+dy_euc = sqrt(dy_mah * dy_mah);
+dz_euc = sqrt(dz_mah * dz_mah);
+dist = sqrt(dx_mah * dx_mah + dy_mah * dy_mah + dz_mah * dz_mah);
+if dist > dtheshold
+    % a_xdamp = 0;
+    %% 若达不到尾流扰动作用范围，通过高斯分布随机生成流场
+    a_xdamp = a_m_o_fixed + a_m_o_sin * sin(t) + a_m_o_rand * (rand(1)*(-2)+1);
+    is_qt_disturb = 0;
+else
+    if dist < x0 
+        % a_xdamp = a_x * (1 - 1/(1 + exp(-dx_euc) + x0 / 2));
+        a_xdamp = -a_x * (1 - 1/(1 + exp(-dx_euc + x0 / 2)));
+    elseif dist == x0
+        a_xdamp = -a_x * (1 - 1/(1 + exp(-dx_euc + x0 / 2)));
+    else
+        a_xdamp = 0;
+    end
+end
+if dist > dtheshold
+    % a_ydamp = 0;
+    %% 若达不到尾流扰动作用范围，通过高斯分布随机生成流场
+    a_ydamp = a_m_o_fixed + a_m_o_sin * sin(t) + a_m_o_rand * (rand(1)*(-2)+1);
+    is_qt_disturb = 0;
+else
+    if dist < y0 || dist == y0
+        if(y_qt > y_auv)
+            a_ydamp = a_y * (1 - 1/(1 + exp(-dy_euc + y0 / 2)));
+        elseif(y_qt < y_auv)
+            a_ydamp = -a_y * (1 - 1/(1 + exp(-dy_euc + y0 / 2)));
+        else
+            a_ydamp = 0;
+        end 
+    else
+        a_ydamp = 0;
+    end
+end
+if dist > dtheshold
+    % a_zdamp = 0;
+    %% 若达不到尾流扰动作用范围，通过高斯分布随机生成流场
+    a_zdamp = a_m_o_fixed + a_m_o_sin * sin(t) + a_m_o_rand * (rand(1)*(-2)+1);
+    is_qt_disturb = 0;
+else
+    if dist < z0 || dist == z0
+        if(z_qt > z_auv)
+            a_zdamp = a_z * (1 - 1/(1 + exp(-dz_euc + z0 / 2)));
+        elseif(z_qt < z_auv)
+            a_zdamp = -a_z * (1 - 1/(1 + exp(-dz_euc + z0 / 2)));
+        else
+            a_zdamp = 0;
+        end 
+    else
+        a_zdamp = 0;
+    end
+end
+
+end %% end of function
---
+++ docking_disturbance_analysis/disturbance_dynamic.m
@@ -0,0 +1,85 @@
+function [ROBNew] = disturbance_dynamic(ROB,deltar,deltab,deltas,tau_dx, tau_dy, tau_dz, tau_dphi, tau_dtheta, tau_dpsi)
+%% 全局变量声明
+%%艇体本体参数
+global m W B x_B y_B z_B x_G y_G z_G I_xx I_yy I_zz;
+%%水动力参数
+global X_dotu Y_dotv Y_dotr Z_dotw Z_dotq K_dotp M_dotw M_dotq N_dotv N_dotr;
+global X_uu Y_vv Y_rr Z_ww Z_qq K_pp M_ww M_qq N_vv N_rr;
+global Y_uv Y_ur Z_uw Z_uq M_uw M_uq N_uv N_ur;
+%%控制力参数
+global Y_uudr Z_uuds Z_uudb M_uuds M_uudb N_uudr;
+global u_const dt;
+%% 艇体状态信息提取
+x = ROB(1);       y = ROB(2);        z = ROB(3);  
+phi = ROB(4);     theta = ROB(5);    psi = ROB(6);  
+u = ROB(7);       v = ROB(8);        w = ROB(9);  
+p = ROB(10);      q = ROB(11);       r = ROB(12);
+
+%% 艇体状态计算
+%垂向运动方程变量替换
+a_zw = m-Z_dotw;     a_zq = -(m*x_G+Z_dotq);
+a_zs = Z_uuds*u*u;   a_zb = Z_uudb*u*u;
+f_z = m*u*q + m*z_G*q*q - X_dotu*u*q + Z_ww*w*abs(w) + Z_uw*u*w + Z_qq*q*abs(q) + Z_uq*u*q + (W-B)*cos(theta);
+%纵倾运动方程变量替换(变量中的t为theta的缩写)
+a_tw = -(m*x_G+M_dotw);  a_tq = I_yy - M_dotq;
+a_ts = M_uuds*u*u;       a_tb = M_uudb*u*u;
+f_t = -m*z_G*w*q - m*x_G*u*q - (Z_dotw*w+Z_dotq*q)*u + X_dotu*u*w + M_ww*w*abs(w) + M_uw*u*w + M_qq*q*abs(q)...
+    +M_uq*u*q - (z_G*W-z_B*B)*sin(theta) - (x_G*W-x_B*B)*cos(theta);
+
+b_z = (a_tq*f_z-a_zq*f_t)/(a_zw*a_tq-a_zq*a_tw);
+b_zb = (a_tq*a_zb-a_zq*a_tb)/(a_zw*a_tq-a_zq*a_tw);
+b_zs = (a_tq*a_zs-a_zq*a_ts)/(a_zw*a_tq-a_zq*a_tw);
+b_t = (a_zw*f_t-a_tw*f_z)/(a_zw*a_tq-a_zq*a_tw);
+b_tb = (a_zw*a_tb-a_tw*a_zb)/(a_zw*a_tq-a_zq*a_tw);
+b_ts = (a_zw*a_ts-a_tw*a_zs)/(a_zw*a_tq-a_zq*a_tw);
+
+d_z = (a_tq*tau_dz-a_zq*tau_dtheta)/(a_zw*a_tq-a_zq*a_tw); % z轴扰动项
+d_theta = (a_zw*tau_dtheta-a_tw*tau_dz)/(a_zw*a_tq-a_zq*a_tw); % 纵倾扰动项
+
+%横向运动方程变量替换(变量中的d为delta的缩写,p为psi的缩写)
+a_yv = m-Y_dotv;  a_yr = m*x_G-Y_dotr;  a_ydr = Y_uudr*u*u;
+f_y = m*y_G*r*r - m*u*r + X_dotu*u*r + Y_vv*v*abs(v) + Y_uv*u*v + Y_rr*r*abs(r) + Y_ur*u*r;
+%横摇运动方程变量替换
+a_pv = m*x_G-N_dotv;  a_pr = I_zz-N_dotr;  a_pdr = N_uudr*u*u;
+f_p = -m*x_G*u*r - m*y_G*v*r + (Y_dotv*v+Y_dotr*r)*u - X_dotu*u*v + N_vv*v*abs(v) + N_uv*u*v + N_rr*r*abs(r) + N_ur*u*r;
+
+b_y = (a_pr*f_y-a_yr*f_p)/(a_yv*a_pr-a_pv*a_yr);
+b_ydr = (a_pr*a_ydr-a_yr*a_pdr)/(a_yv*a_pr-a_pv*a_yr);
+b_p = (a_yv*f_p-a_pv*f_y)/(a_yv*a_pr-a_pv*a_yr);
+b_pdr = (a_yv*a_pdr-a_pv*a_ydr)/(a_yv*a_pr-a_pv*a_yr);
+
+d_y = -(a_pr*tau_dy-a_yr*tau_dpsi)/(a_yv*a_pr-a_pv*a_yr); % y轴扰动项
+d_psi = -(a_yv*tau_dpsi-a_pv*tau_dy)/(a_yv*a_pr-a_pv*a_yr);% 偏航扰动项
+
+%% 艇体状态更新
+ROBdot_x = u*cos(psi)*cos(theta) - v*sin(psi) + w*cos(psi)*sin(theta);
+ROBdot_y = u*sin(psi) + v*cos(psi);
+ROBdot_z = -u*sin(theta) + w*cos(theta);
+ROBdot_phi = 0;
+ROBdot_theta = q;
+ROBdot_psi = r; 
+ROBdot_u = 0;
+% ROBdot_v = b_y + b_ydr*deltar; 
+ROBdot_v = b_y + b_ydr*deltar + d_y; % 简化水平面运动状态加入扰动项
+% ROBdot_w = b_z + b_zb*deltab + b_zs*deltas; 
+ROBdot_w = b_z + b_zb*deltab + b_zs*deltas + d_z; % 简化垂直面运动状态加入扰动项
+ROBdot_p = 0; 
+% ROBdot_q = b_t + b_tb*deltab + b_ts*deltas; 
+ROBdot_q = b_t + b_tb*deltab + b_ts*deltas + d_theta;% 简化垂直面运动状态加入扰动项
+% ROBdot_r = b_p + b_pdr*deltar;  
+ROBdot_r = b_p + b_pdr*deltar + d_psi; % 简化水平面运动状态加入扰动项
+
+x = x + ROBdot_x*dt;
+y = y + ROBdot_y*dt;
+z = z + ROBdot_z*dt;
+phi = 0;
+theta = theta + ROBdot_theta*dt;
+psi = psi + ROBdot_psi*dt;
+u = u_const;
+v = v + ROBdot_v*dt;
+w = w + ROBdot_w*dt;
+p = 0;
+q = q + ROBdot_q*dt;
+r = r + ROBdot_r*dt;
+ROBNew = [x,y,z,phi,theta,psi,u,v,w,p,q,r];
+end
